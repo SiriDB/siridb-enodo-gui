1f16d5889390e1bf00e5b01b3e0a819af016854d
Rob <rob.vd.boom@hotmail.com>
2021-03-12 16:06:20 +0100

    Added alert in series configurator

--- app/src/components/Serie/Edit.js
+++ app/src/components/Serie/Edit.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import SerieConfigurator from './SerieConfigurator';
 import { socket } from '../../store';
 
 function EditSerie({ close, currentSerie }) {
 
+    const [socketError, setSocketError] = useState(null);
+
     return (
         <SerieConfigurator
             title={"Edit serie"}
             onSubmit={(data) => {
-                socket.emit('/api/series/update', data);
-                close();
+                socket.emit('/api/series/update', data, (data) => {
+                    const value = JSON.parse(data);
+                    if (value.error) {
+                        setSocketError(value.error);
+                    }
+                    else {
+                        close();
+                    }
+                });
             }}
             onClose={close}
             dialog='edit'
             currentSerie={currentSerie}
+            socketError={socketError}
         />
     )
 };
