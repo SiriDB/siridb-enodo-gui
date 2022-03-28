e1cb2b61a150078c360ed5f38b1e4de102162b65
rob <rob.vd.boom@hotmail.com>
2022-03-28 16:22:59 +0200

    Updated packages

--- app/src/index.js
+++ app/src/index.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
+import React from "react";
+import ReactDOM from "react-dom";
+import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
 
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import App from "./App";
 
 const theme = createTheme({
   palette: {
-    type: 'light',
+    type: "light",
     contrastThreshold: 3,
     primary: {
-      main: '#09bbad',
-      contrastText: '#fff'
-    }
-  }
+      main: "#09bbad",
+      contrastText: "#fff",
+    },
+  },
 });
 
 ReactDOM.render(
@@ -23,10 +22,5 @@ ReactDOM.render(
       <App />
     </MuiThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
