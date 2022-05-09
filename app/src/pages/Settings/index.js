b0f78589289b15d460c531d3275e46e07404d25a
timoj <timoj@users.noreply.github.com>
2022-05-09 11:28:38 +0200

    upd settings page

--- app/src/pages/Settings/index.js
+++ app/src/pages/Settings/index.js
@@ -27,19 +27,14 @@ const useStyles = makeStyles((theme) => ({
 const OutputStreamsPage = ({ siridb_status, socket }) => {
   const classes = useStyles();
   const [settings, setSettings] = useState(null);
-  const [siridb, setSiridb] = useState(null);
-  const [siridbOutput, setSiridbOutput] = useState(null);
-  const [sameDatabaseChecked, setSameDatabaseChecked] = useState(false);
+  const [eventSettings, setEventSettings] = useState(null);
+  const [analyserSettings, setAnalyserSettings] = useState(null);
 
   const retrieveSettings = useCallback(() => {
     socket.emit("/api/enodo/settings", {}, (data) => {
       setSettings(data.data);
-      setSiridb(data.data.siridb);
-      setSiridbOutput(data.data.siridb_output);
-      setSameDatabaseChecked(
-        JSON.stringify(data.data.siridb) ===
-          JSON.stringify(data.data.siridb_output)
-      );
+      setEventSettings(data.data.events);
+      setAnalyserSettings(data.data.analyser);
     });
   }, [socket]);
 
@@ -47,124 +42,66 @@ const OutputStreamsPage = ({ siridb_status, socket }) => {
     retrieveSettings();
   }, [retrieveSettings]);
 
-  const toggleCheckbox = () => {
-    const newValue = !sameDatabaseChecked;
-    setSameDatabaseChecked(newValue);
-    if (newValue) {
-      setSiridbOutput(siridb);
-      saveSettings({
-        section: "siridb_output",
-        entries: siridb,
-      });
-    }
-  };
-
   const saveSettings = (data) => {
-    socket.emit(`/api/enodo/settings/update`, data, () => {
+    let tmp_settings = {
+      "section": "events",
+      "entries": eventSettings
+    };
+    socket.emit(`/api/enodo/settings/update`, tmp_settings, () => {
+      retrieveSettings();
+    });
+    tmp_settings = {
+      "section": "analyser",
+      "entries": analyserSettings
+    };
+    socket.emit(`/api/enodo/settings/update`, tmp_settings, () => {
       retrieveSettings();
     });
   };
 
-  const handleChange = (target, databaseType) => {
-    if (databaseType === "siridb") {
-      setSiridb((s) => ({ ...s, [target.name]: target.value }));
-    } else {
-      setSiridbOutput((s) => ({ ...s, [target.name]: target.value }));
+  const handleChange = (target, settingsSection) => {
+    if (settingsSection === "events") {
+      setEventSettings((s) => ({ ...s, [target.name]: target.value }));
+    } else if (settingsSection === "analyser") {
+      setAnalyserSettings((s) => ({ ...s, [target.name]: target.value }));
     }
   };
 
   return (
     <BasicPageLayout title="Settings">
-      {settings && siridb && siridbOutput ? (
+      {settings && eventSettings && analyserSettings ? (
         <Paper className={classes.paper}>
           <Grid container direction="column" spacing={3}>
             <Grid item>
-              <Typography variant="h5" gutterBottom>
-                {"SiriDB connections"}
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Typography variant="subtitle2">{"Analysis database"}</Typography>
+              <Typography variant="subtitle2">{"Runtime settings"}</Typography>
             </Grid>
             <Grid item>
               <Grid container direction="row" spacing={3}>
                 <Grid item xs={6}>
-                  <Grid container spacing={2} direction="column">
+                  <Grid container spacing={3} direction="column">
                     <Grid item>
                       <TextField
-                        placeholder="Host"
-                        name="host"
-                        onChange={(e) => handleChange(e.target, "siridb")}
+                        label="Max queue length before event"
+                        placeholder="Max queue length before event"
+                        name="max_in_queue_before_warning"
+                        onChange={(e) => handleChange(e.target, "events")}
                         variant="outlined"
                         className={classes.textField}
                         type="text"
-                        value={siridb.host}
+                        value={eventSettings.max_in_queue_before_warning}
                       />
                     </Grid>
                     <Grid item>
                       <TextField
-                        placeholder="Port"
-                        name="port"
-                        onChange={(e) => handleChange(e.target, "siridb")}
-                        variant="outlined"
-                        className={classes.textField}
-                        type="number"
-                        value={siridb.port}
-                      />
-                    </Grid>
-                    <Grid item>
-                      <TextField
-                        placeholder="Username"
-                        name="user"
-                        onChange={(e) => handleChange(e.target, "siridb")}
+                        label="Min data points (used when not specified in series config)"
+                        placeholder="Min data points"
+                        name="min_data_points"
+                        onChange={(e) => handleChange(e.target, "analyser")}
                         variant="outlined"
                         className={classes.textField}
                         type="text"
-                        value={siridb.user}
+                        value={analyserSettings.min_data_points}
                       />
-                    </Grid>
-                    <Grid item>
-                      <TextField
-                        placeholder="Password"
-                        name="password"
-                        onChange={(e) => handleChange(e.target, "siridb")}
-                        variant="outlined"
-                        className={classes.textField}
-                        type="text"
-                        value={siridb.password}
-                      />
-                    </Grid>
-                    <Grid item>
-                      <TextField
-                        placeholder="Database name"
-                        name="database"
-                        onChange={(e) => handleChange(e.target, "siridb")}
-                        variant="outlined"
-                        className={classes.textField}
-                        type="text"
-                        value={siridb.database}
-                      />
-                    </Grid>
-                    <Grid item>
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={() => {
-                          saveSettings({
-                            section: "siridb",
-                            entries: siridb,
-                          });
-                          if (sameDatabaseChecked) {
-                            saveSettings({
-                              section: "siridb_output",
-                              entries: siridb,
-                            });
-                          }
-                        }}
-                        disabled={siridb === settings["siridb"]}
-                      >
-                        {"Save"}
-                      </Button>
                     </Grid>
                   </Grid>
                 </Grid>
@@ -196,16 +133,13 @@ const OutputStreamsPage = ({ siridb_status, socket }) => {
                         }
                       >
                         {siridb_status.data_conn
-                          ? "You are connected"
-                          : "You are not connected"}
+                          ? "Connected to SiriDB"
+                          : "Not connected to SiriDB"}
                       </Typography>
                     </Grid>
                   </Grid>
                 </Grid>
               </Grid>
-            </Grid>
-            <Grid item>
-              <Divider />
             </Grid>
             <Grid item>
               <Grid container direction="row" spacing={3}>
@@ -214,153 +148,20 @@ const OutputStreamsPage = ({ siridb_status, socket }) => {
                     container
                     direction="row"
                     justifyContent="space-between"
-                    alignItems="center"
-                  >
-                    <Grid item>
-                      <Typography variant="subtitle2">
-                        {"Output database"}
-                      </Typography>
-                    </Grid>
-                    <Grid item>
-                      <FormControlLabel
-                        control={
-                          <Checkbox
-                            checked={sameDatabaseChecked}
-                            onChange={toggleCheckbox}
-                            color="primary"
-                          />
-                        }
-                        label="Is the same as the analysis database"
-                      />
+                    spacing={2}>
+                    <Grid item xs={12}>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => {
+                          saveSettings();
+                        }}
+                      >
+                        {"Save"}
+                      </Button>
                     </Grid>
                   </Grid>
                 </Grid>
-                {!sameDatabaseChecked && (
-                  <React.Fragment>
-                    <Grid item xs={6}>
-                      <Grid container spacing={2} direction="column">
-                        <Grid item>
-                          <TextField
-                            placeholder="Host"
-                            name="host"
-                            onChange={(e) =>
-                              handleChange(e.target, "siridb_output")
-                            }
-                            variant="outlined"
-                            className={classes.textField}
-                            type="text"
-                            value={siridbOutput.host}
-                          />
-                        </Grid>
-                        <Grid item>
-                          <TextField
-                            placeholder="Port"
-                            name="port"
-                            onChange={(e) =>
-                              handleChange(e.target, "siridb_output")
-                            }
-                            variant="outlined"
-                            className={classes.textField}
-                            type="number"
-                            value={siridbOutput.port}
-                          />
-                        </Grid>
-                        <Grid item>
-                          <TextField
-                            placeholder="Username"
-                            name="user"
-                            onChange={(e) =>
-                              handleChange(e.target, "siridb_output")
-                            }
-                            variant="outlined"
-                            className={classes.textField}
-                            type="text"
-                            value={siridbOutput.user}
-                          />
-                        </Grid>
-                        <Grid item>
-                          <TextField
-                            placeholder="Password"
-                            name="password"
-                            onChange={(e) =>
-                              handleChange(e.target, "siridb_output")
-                            }
-                            variant="outlined"
-                            className={classes.textField}
-                            type="text"
-                            value={siridbOutput.password}
-                          />
-                        </Grid>
-                        <Grid item>
-                          <TextField
-                            placeholder="Database name"
-                            name="database"
-                            onChange={(e) =>
-                              handleChange(e.target, "siridb_output")
-                            }
-                            variant="outlined"
-                            className={classes.textField}
-                            type="text"
-                            value={siridbOutput.database}
-                          />
-                        </Grid>
-                        <Grid item>
-                          <Button
-                            variant="contained"
-                            color="primary"
-                            onClick={() =>
-                              saveSettings({
-                                section: "siridb_output",
-                                entries: siridbOutput,
-                              })
-                            }
-                            disabled={
-                              siridbOutput === settings["siridb_output"]
-                            }
-                          >
-                            {"Save"}
-                          </Button>
-                        </Grid>
-                      </Grid>
-                    </Grid>
-                    <Grid item xs={6} style={{ display: "flex" }}>
-                      <Grid
-                        container
-                        justifyContent="center"
-                        alignItems="center"
-                        direction="column"
-                      >
-                        <Grid item>
-                          {siridb_status.analysis_conn ? (
-                            <CloudDoneIcon
-                              color="primary"
-                              style={{ fontSize: 80 }}
-                            />
-                          ) : (
-                            <CloudOffIcon
-                              color="disabled"
-                              style={{ fontSize: 80 }}
-                            />
-                          )}
-                        </Grid>
-                        <Grid item>
-                          <Typography
-                            variant="h6"
-                            color={
-                              siridb_status.analysis_conn
-                                ? "primary"
-                                : "textSecondary"
-                            }
-                          >
-                            {siridb_status.analysis_conn
-                              ? "You are connected"
-                              : "You are not connected"}
-                          </Typography>
-                        </Grid>
-                      </Grid>
-                    </Grid>
-                  </React.Fragment>
-                )}
               </Grid>
             </Grid>
           </Grid>
